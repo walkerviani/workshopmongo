2e9928a148d23d6e091d8df583dc13f330b7c718
Walker Viani <walkeryslan@gmail.com>
2026-01-01 20:21:25 -0300

    "Returning users using the DTO pattern"

--- src/main/java/com/walkerviani/workshopmongo/resources/UserResource.java
+++ src/main/java/com/walkerviani/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.walkerviani.workshopmongo.resources;
 
 import com.walkerviani.workshopmongo.domain.User;
+import com.walkerviani.workshopmongo.dto.UserDTO;
 import com.walkerviani.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,8 +22,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }

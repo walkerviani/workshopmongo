dad0bbbcf8fbedd8d8e286cf5008ffa2fdb0a61f
Walker Viani <walkeryslan@gmail.com>
2026-01-02 13:37:50 -0300

    "Create Post with Nested User"

---
+++ src/main/java/com/walkerviani/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.walkerviani.workshopmongo.repository;
+
+import com.walkerviani.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+}

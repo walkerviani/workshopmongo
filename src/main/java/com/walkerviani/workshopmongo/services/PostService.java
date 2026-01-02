7b066186ffff1e350be2bae183545b2244e0cccc
Walker Viani <walkeryslan@gmail.com>
2026-01-02 14:43:50 -0300

    "Find post by ID"

---
+++ src/main/java/com/walkerviani/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.walkerviani.workshopmongo.services;
+
+import com.walkerviani.workshopmongo.domain.Post;
+import com.walkerviani.workshopmongo.repository.PostRepository;
+import com.walkerviani.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}

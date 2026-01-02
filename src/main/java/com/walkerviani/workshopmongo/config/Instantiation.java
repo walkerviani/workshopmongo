10357823640aa1764e0aa39db8679d89a2b8a901
Walker Viani <walkeryslan@gmail.com>
2026-01-02 14:21:09 -0300

    "Associating posts with the user"

--- src/main/java/com/walkerviani/workshopmongo/config/Instantiation.java
+++ src/main/java/com/walkerviani/workshopmongo/config/Instantiation.java
@@ -42,5 +42,7 @@ public class Instantiation implements CommandLineRunner {
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
